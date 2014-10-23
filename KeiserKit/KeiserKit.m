c6d07ddb11d8e5d316b04b60d1dea4f49186bd55
Ben Woodford <me@benwoodford.co.uk>
2014-10-23 12:33:18 +0100

    Initial Commit

---
+++ KeiserKit/KeiserKit.m
@@ -0,0 +1,13 @@
+//
+//  KeiserKit.m
+//  KeiserKit
+//
+//  Created by Ben Woodford on 23/10/2014.
+//  Copyright (c) 2014 Ben Woodford. All rights reserved.
+//
+
+#import "KeiserKit.h"
+
+@implementation KeiserKit
+
+@end
